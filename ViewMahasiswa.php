a4b81e43086a8f0945c5872f4f54991e6d8581e2
Rickard Elsen <ainx@ainx.gosc.info>
2015-06-12 00:36:47 +0700

    lengkapi crud standar untuk mahasiswa

---
+++ ViewMahasiswa.php
@@ -0,0 +1,181 @@
+<?php
+session_start();
+session_destroy();
+session_start();
+$_SESSION['status']="view";
+include 'controller/Mahasiswa.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Log ITB</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="description" content="" />
+<meta name="author" content="http://bootstraptaste.com" />
+<!-- css -->
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+<link href="css/fancybox/jquery.fancybox.css" rel="stylesheet">
+<link href="css/jcarousel.css" rel="stylesheet" />
+<link href="css/flexslider.css" rel="stylesheet" />
+<link href="css/style.css" rel="stylesheet" />
+
+
+<!-- Theme skin -->
+<link href="skins/default.css" rel="stylesheet" />
+
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+<div id="wrapper">
+	<!-- start header -->
+	<header>
+        <div class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand logo" href="index.html"><img src="img/logo-itb.png" alt="" /></a>
+                </div>
+                <div class="navbar-collapse collapse ">
+                    <ul class="nav navbar-nav">
+                        <li><a href="index.html">Home</a></li>
+                        <li class="active"><a href="registrasi.html">Registrasi</a></li>
+                        <li><a href="contact.html">Hubungi Kami</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+	</header>
+	<!-- end header -->
+	<section id="inner-headline">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <ul class="breadcrumb">
+                            <li><a href="#"><i class="fa fa-home"></i></a><i class="icon-angle-right"></i></li>
+                            <li class="active">Form Registrasi</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+	</section>
+	<section id="content">
+            <div class="container">
+		<div class="row">
+                    <div class="col-lg-2"></div>
+                    <div class="col-lg-8">
+                        <table class="table table-bordered">
+                            <tr>
+                                <th>NIM</th>
+                                <th>Nama</th>
+                                <th>Email</th>
+                                <th>Alamat</th>
+                                <th>No. Telp</th>
+                                <th>Aksi</th>
+                            </tr>
+                            <?php
+                                $val = $_SESSION['value'];
+                                $x = count($val['nim']);
+                                for($i=0;$i<$x;$i++){
+                                    echo "<tr>";
+                                    echo "<td>".$val['nim'][$i]."</td>";
+                                    echo "<td>".$val['nama'][$i]."</td>";
+                                    echo "<td>".$val['email'][$i]."</td>";
+                                    echo "<td>".$val['alamat'][$i]."</td>";
+                                    echo "<td>".$val['telp'][$i]."</td>";
+                                    echo "<td>";
+                                    echo "<form action=\"controller/Mahasiswa.php\" method=\"POST\" enctype=\"multipart/form-data\">";
+                                    echo "<input type=\"hidden\" name=\"nim\" value=".$val['nim'][$i]." class=\"btn btn-blue\" />";
+                                    echo "<input type=\"submit\" name=\"edit\" value=\"Edit\" class=\"btn btn-green\" />";
+                                    echo "<input type=\"submit\" name=\"delete\" value=\"Delete\" class=\"btn btn-red\" />";
+                                    echo "</form>";
+                                    echo "</td>";
+                                    echo "</tr>";
+                                }
+                            ?>
+                        </table>
+                    </div>
+                    <div class="col-lg-2"></div>
+		</div>
+            </div>
+	</section>
+	<footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3">
+<!--                        <div class="widget">
+                            <h5 class="widgetheading">Info Lebih Lanjut</h5>
+                            <address>
+                                <strong>Contact Person: Dian Ramadhani</strong><br>
+                                Institut Teknologi Bandung<br>
+                                Labtek V Jl. Ganesha 10 Bandung
+                            </address>
+                            <p>
+                                <i class="icon-phone"></i> +62-85265863357 (a.n. Dian Ramadhani) <br/>
+                                <i class="icon-phone"></i> +62-22-2508135 (a.n. Fazat Nur Azizah)<br/>
+                                Fax: +62-22-2500940 <br/>
+                                <i class="icon-envelope-alt"></i> dse@informatika.org <br/>
+                                Facebook fan page	: DSE Days
+                            </p>
+                        </div>-->
+                    </div>
+                    <div class="col-lg-3">
+<!--                        <div class="widget">
+                            <h5 class="widgetheading">Pages</h5>
+                            <ul class="link-list">
+                                <li><a href="index.html">Home</a></li>
+                                <li><a href="registrasi.html">Registrasi</a></li>
+                                <li><a href="contact.html">Hubungi Kami</a></li>
+                            </ul>
+                        </div>-->
+                    </div>
+                    <div class="col-lg-3">
+<!--                        <div class="widget">
+                            <h5 class="widgetheading">Kegiatan DSE Days</h5>
+                            <ul class="link-list">
+                                <li>Tutorial on Software Engineering in DevOps Context</li>
+                                <li>Konferensi Nasional Rekayasa Data</li>
+                                <li>Seminar dan Workshop Pendidikan Rekayasa Perangkat Lunak</li>
+                                <li>Tutorial on Data Analytics and Visualization</li>
+                            </ul>
+                        </div>-->
+                    </div>
+                    <div class="col-lg-3">
+<!--                        <div class="widget">
+                            <h5 class="widgetheading">Link</h5>
+                            <div class="flickr_badge">
+                                <script type="text/javascript" src="http://www.flickr.com/badge_code_v2.gne?count=8&amp;display=random&amp;size=s&amp;layout=x&amp;source=user&amp;user=34178660@N03"></script>
+                            </div>
+                            <div class="clear">
+                            </div>
+                        </div>-->
+                    </div>
+                </div>
+            </div>
+	</footer>
+</div>
+<a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
+<!-- javascript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="js/jquery.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.fancybox.pack.js"></script>
+<script src="js/jquery.fancybox-media.js"></script>
+<script src="js/google-code-prettify/prettify.js"></script>
+<script src="js/portfolio/jquery.quicksand.js"></script>
+<script src="js/portfolio/setting.js"></script>
+<script src="js/jquery.flexslider.js"></script>
+<script src="js/animate.js"></script>
+<script src="js/custom.js"></script>
+</body>
+</html>
--- controller/Mahasiswa.php
+++ controller/Mahasiswa.php
@@ -1,16 +1,41 @@
 <!--Mahasiswa Controller-->
 <?php
-    include '../model/Mahasiswa.php';
-    session_start();
+    include $_SERVER['DOCUMENT_ROOT'].'/log-itb/model/Mahasiswa.php';
+    
     if(isset($_POST['add'])){
         $r = insert($_POST['nim'], $_POST['nama'], $_POST['email'], $_POST['alamat'], $_POST['telp']);
         if($r){
+            session_start();
             $_SESSION['success']="Berhasil tambah mahasiswa.. :)";
             header('Location: ../AddMahasiswa.php');
         }
     }else{
-        echo "ga ada data";
+        
+    }
+    if(isset($_SESSION['status'])){
+        $_SESSION['value'] = view();
     }
     
+    if(isset($_POST['edit'])){
+        $r = viewByNim($_POST['nim']);
+      
+        session_start();
+        $_SESSION['data']=$r;
+        header('Location: ../EditMahasiswa.php');
+    }
+    
+    if(isset($_POST['update'])){
+        $r = update($_POST['nim'], $_POST['nama'], $_POST['email'], $_POST['alamat'], $_POST['telp']);
+        if($r){
+            header('Location: ../ViewMahasiswa.php');
+        }
+    }
+    
+    if(isset($_POST['delete'])){
+        $r = delete($_POST['nim']);
+        if($r){
+            header('Location: ../ViewMahasiswa.php');
+        }
+    }
 ?>
 
