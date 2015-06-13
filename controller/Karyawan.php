3bcc7e6be99c835640125f999ae64f829c8c555c
Elia Dolaciho Bangun <eltheuno@gmail.com>
2015-06-12 19:00:48 -0700

    + CRUD Data Karyawan

---
+++ controller/Karyawan.php
@@ -0,0 +1,41 @@
+<!--Karyawan Controller-->
+<?php
+
+ include $_SERVER['DOCUMENT_ROOT'].'/LogITB/model/Karyawan.php';
+    
+    if(isset($_POST['add'])){
+        $r = insert($_POST['nip'], $_POST['nama'], $_POST['email'], $_POST['alamat'], $_POST['telp']);
+        if($r){
+            $_SESSION['success']="Berhasil tambah Karyawan.. :)";
+            header('Location: ../AddKaryawan.php');
+        }
+    }else{
+        //echo "ga ada data";
+    }
+    
+    if(isset($_SESSION['status'])){
+        $_SESSION['value'] = view();
+    }
+    
+    if(isset($_POST['edit'])){
+        $r = viewByNip($_POST['nip']);
+      
+        session_start();
+        $_SESSION['data']=$r;
+        header('Location: ../EditKaryawan.php');
+    }
+    
+    if(isset($_POST['update'])){
+        $r = update($_POST['nip'], $_POST['nama'], $_POST['email'], $_POST['alamat'], $_POST['telp']);
+        if($r){
+            header('Location: ../ViewKaryawan.php');
+        }
+    }
+    
+    if(isset($_POST['delete'])){
+        $r = delete($_POST['nip']);
+        if($r){
+            header('Location: ../ViewKaryawan.php');
+        }
+    }
+?>
---
+++ model/Karyawan.php
@@ -0,0 +1,51 @@
+<!--Karyawan Model-->
+<?php
+    
+    function insert ($nip,$nama,$email,$alamat,$telp){
+        include '../db.php';
+        $sql = "INSERT INTO Karyawan VALUES ('".$nip."','".$nama."','".$email."','".$alamat."','".$telp."')";
+        return mysqli_query($link,$sql);
+    }
+    
+    function view(){
+        include $_SERVER['DOCUMENT_ROOT'].'/LogITB/db.php';
+        $sql = "SELECT * FROM Karyawan";
+        $res = mysqli_query($link, $sql);
+        $i=0;
+        while($r = mysqli_fetch_assoc($res)){
+            $value['nip'][$i]=$r['nip'];
+            $value['nama'][$i]=$r['nama'];
+            $value['email'][$i]=$r['email'];
+            $value['alamat'][$i]=$r['alamat'];
+            $value['telp'][$i]=$r['noTelp'];
+            $i++;
+        }
+        return $value;
+    }
+    function viewByNip($nip){
+        include $_SERVER['DOCUMENT_ROOT'].'/LogITB/db.php';
+        $sql = "SELECT * FROM Karyawan WHERE nip='".$nip."'";
+        $res = mysqli_query($link, $sql);
+        $i=0;
+        if($r = mysqli_fetch_assoc($res)){
+            $value['nip']=$r['nip'];
+            $value['nama']=$r['nama'];
+            $value['email']=$r['email'];
+            $value['alamat']=$r['alamat'];
+            $value['telp']=$r['noTelp'];
+            $i++;
+        }
+        return $value;
+    }
+    function update ($nip,$nama,$email,$alamat,$telp){
+        include $_SERVER['DOCUMENT_ROOT'].'/LogITB/db.php';
+        $sql = "UPDATE Karyawan SET nama='".$nama."',email='".$email."',alamat='".$alamat."',noTelp='".$telp."' WHERE nip='".$nip."'";
+        return mysqli_query($link,$sql);
+    }
+    function delete ($nip){
+        include $_SERVER['DOCUMENT_ROOT'].'/LogITB/db.php';
+        $sql = "DELETE FROM Karyawan WHERE nip='".$nip."'";
+        return mysqli_query($link,$sql);
+    }
+    
+?>
