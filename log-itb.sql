11d55e3797e131140bc1549d6731a5f59f41afbe
Elia Dolaciho Bangun <eltheuno@gmail.com>
2015-06-13 10:57:51 -0700

    + update log-itb.sql

--- log-itb.sql
+++ log-itb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.9
+-- version 4.3.11
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Jun 10, 2015 at 03:55 AM
--- Server version: 5.6.25
--- PHP Version: 5.6.9
+-- Host: 127.0.0.1
+-- Generation Time: 13 Jun 2015 pada 19.56
+-- Versi Server: 5.6.24
+-- PHP Version: 5.6.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `log-itb`
@@ -23,10 +23,52 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Mahasiswa`
+-- Struktur dari tabel `dosen`
 --
 
-CREATE TABLE IF NOT EXISTS `Mahasiswa` (
+CREATE TABLE IF NOT EXISTS `dosen` (
+  `nip` varchar(8) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `alamat` text NOT NULL,
+  `noTelp` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `dosen`
+--
+
+INSERT INTO `dosen` (`nip`, `nama`, `email`, `alamat`, `noTelp`) VALUES
+('12345678', 'Dosen Saya', 'dosensaya@gmail.com', 'Jl.Dimana saja', '08293939301');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `karyawan`
+--
+
+CREATE TABLE IF NOT EXISTS `karyawan` (
+  `nip` varchar(8) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `alamat` text NOT NULL,
+  `noTelp` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `karyawan`
+--
+
+INSERT INTO `karyawan` (`nip`, `nama`, `email`, `alamat`, `noTelp`) VALUES
+('12312312', 'Siapakah dia 1', 'siapakh@siapa.com', 'Jln.Dimanakah', '292929292123123');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE IF NOT EXISTS `mahasiswa` (
   `nim` varchar(8) NOT NULL,
   `nama` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
@@ -35,11 +77,92 @@ CREATE TABLE IF NOT EXISTS `Mahasiswa` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `Mahasiswa`
+-- Dumping data untuk tabel `mahasiswa`
 --
 
-INSERT INTO `Mahasiswa` (`nim`, `nama`, `email`, `alamat`, `noTelp`) VALUES
-('23513030', 'Rickard Elsen', 'rickardelsen@gmail.com', 'Dimana Aja', '022 2512356');
+INSERT INTO `mahasiswa` (`nim`, `nama`, `email`, `alamat`, `noTelp`) VALUES
+('23513030', 'Rickard Elsen', 'rickardelsen@gmail.com', 'Dimana Aja', '022 2512356'),
+('23513070', 'Elia Dolaciho Bangun', 'eliad.bangun@gmail.com', 'Jl. Pelindung Hewan No.8D', '087822233260');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `role`
+--
+
+CREATE TABLE IF NOT EXISTS `role` (
+  `idrole` varchar(30) NOT NULL,
+  `deskripsi` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `role`
+--
+
+INSERT INTO `role` (`idrole`, `deskripsi`) VALUES
+('Admin', 'Role yang berfungsi mengelola data user.111');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `username` varchar(8) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `userrole`
+--
+
+CREATE TABLE IF NOT EXISTS `userrole` (
+  `username` varchar(8) NOT NULL,
+  `idrole` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `dosen`
+--
+ALTER TABLE `dosen`
+  ADD PRIMARY KEY (`nip`);
+
+--
+-- Indexes for table `karyawan`
+--
+ALTER TABLE `karyawan`
+  ADD PRIMARY KEY (`nip`);
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`nim`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`idrole`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `userrole`
+--
+ALTER TABLE `userrole`
+  ADD PRIMARY KEY (`username`,`idrole`), ADD KEY `IdRole` (`idrole`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
