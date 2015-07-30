815d77fc5d1d31cd061d7e97f9197b29b7eaa5a0
Elia Dolaciho Bangun <eltheuno@gmail.com>
2015-07-30 10:42:01 +0700

    + Modif Frontend

--- InformasiMahasiswa.php
+++ InformasiMahasiswa.php
@@ -37,8 +37,8 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <ul class="breadcrumb">
-                            <li><a href="#"><i class="fa fa-home"></i></a><i class="icon-angle-right"></i></li>
-                            <li class="active">Home</li>
+                             <li><a href="index.php"><i class="fa fa-home"></i></a><i class="icon-angle-right"></i></li>
+                            <li class="active">Informasi Mahasiswa</li>
                         </ul>
                     </div>
                 </div>
--- index.php
+++ index.php
@@ -43,8 +43,8 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <ul class="breadcrumb">
-                            <li><a href="#"><i class="fa fa-home"></i></a><i class="icon-angle-right"></i></li>
-                            <li class="active">Home</li>
+                            <li><a href="index.php"><i class="fa fa-home"></i></a><i class="icon-angle-right"></i></li>
+                            <li class="active">Login Form</li>
                         </ul>
                     </div>
                 </div>
