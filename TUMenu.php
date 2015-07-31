ba58d70545022446feae78498a3508719d18863c
Elia Dolaciho Bangun <eltheuno@gmail.com>
2015-07-31 09:18:08 +0700

    + Modif TUMenu

--- TUMenu.php
+++ TUMenu.php
@@ -4,18 +4,7 @@
 <h3>Menu</h3>
 <div id="cssmenu">
     <ul>
-        
-        <li class="has-sub"><a href='#'><span class = "menu" >SK Bimbingan</span></a>
-            <ul>
-                <li>
-                    <a href="AddSK.php">Add SK</a>
-                </li>
-                <li>
-                    <a href="ViewSK.php">View SK</a>
-                </li>
-            </ul>
-        </li>
-        <li class="has-sub"><a href='#'><span class = "menu" >Mata Kuliah</span></a>
+        <li class="has-sub"><a href='#'><span>Mata Kuliah</span></a>
             <ul>
                 <li>
                     <a href="AddMataKuliah.php">Add Mata Kuliah</a>
@@ -25,7 +14,8 @@
                 </li> 
             </ul>
         </li>
-        <li class="has-sub"><a href='#'><span class = "menu" >Pengumuman</span></a>
+        
+        <li class="has-sub"><a href='#'><span >Pengumuman</span></a>
             <ul>
                 <li>
                     <a href="AddPengumuman.php">Add Pengumuman</a>
@@ -35,7 +25,7 @@
                 </li>
             </ul>
         </li>
-        <li class="has-sub"><a href='#'><span class = "menu" >Wali Mahasiswa</span></a>
+        <li class="has-sub"><a href='#'><span >Wali Mahasiswa</span></a>
             <ul>
                 <li>
                     <a href="AddWaliMahasiswa.php">Add Wali Mahasiswa</a>
@@ -43,6 +33,68 @@
                 <li>
                     <a href="ViewWaliMahasiswa.php">view Wali Mahasiswa</a>
                 </li>
+            </ul>
+        </li>
+        <li class="has-sub"><a href='#'><span >Periode TA</span></a>
+            <ul>
+                <li>
+                    <a href="AddPeriodeTA.php">Add Periode TA</a>
+                </li>
+                <li>
+                    <a href="ViewPeriodeTA.php">view Periode TA</a>
+                </li>
+            </ul>
+        </li>
+        <li class="has-sub"><a href='#'><span>Pengambilan Mata Kuliah</span></a>
+            <ul>
+                <li>
+                    <a href="AddPengambilanMataKuliah.php">Add Pengambilan Mata Kuliah</a>
+                </li>
+                <li>
+                    <a href="ViewPengambilanMataKuliah.php">View Pengambilan Mata Kuliah</a>
+                </li> 
+            </ul>
+        </li>
+        <li class="has-sub"><a href='#'><span>SK Bimbingan</span></a>
+            <ul>
+                <li>
+                    <a href="AddSKBimbingan.php">Add SK Bimbingan</a>
+                </li>
+                <li>
+                    <a href="ViewSKBimbingan.php">View SK Bimbingan</a>
+                </li> 
+            </ul>
+        </li>
+        <li class="has-sub"><a href='#'><span>SK Sidang</span></a>
+            <ul>
+                <li>
+                    <a href="AddSKSidang.php">Add SK Sidang</a>
+                </li>
+                <li>
+                    <a href="ViewSKSidang.php">View SK Sidang</a>
+                </li> 
+            </ul>
+        </li>
+        <li class="has-sub"><a href='#'><span>History Judul TA</span></a> 
+        </li>
+        <li class="has-sub"><a href='#'><span>Seminar Tugas Akhir 1</span></a>
+            <ul>
+                <li>
+                    <a href="AddSeminarTA1.php">Add Seminar Tugas Akhir 1</a>
+                </li>
+                <li>
+                    <a href="ViewSeminarTA1.php">View Seminar Tugas Akhir 1</a>
+                </li> 
+            </ul>
+        </li>
+        <li class="has-sub"><a href='#'><span>Nilai Detail TA 1</span></a>
+            <ul>
+                <li>
+                    <a href="AddNilaiDetailTA1.php">Add Nilai Detail TA 1</a>
+                </li>
+                <li>
+                    <a href="ViewNilaiDetailTA1.php">View Nilai Detail TA 1</a>
+                </li> 
             </ul>
         </li>
     </ul>
